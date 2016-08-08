8738ad24db8419d56fed25205653f9a978b99571
Matt Isenhower <matt@isenhower.com>
2016-08-08 09:50:40 -0700

    Initial version

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var webpack = require('webpack-stream');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', ['styles', 'fonts', 'scripts']);
+
+gulp.task('styles', function() {
+	return gulp.src('sass/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src('node_modules/bootstrap-sass/assets/fonts/**/*')
+		.pipe(gulp.dest('./public/fonts/'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('scripts/main.js')
+		.pipe(webpack({output: {filename: 'main.js'}}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('watch', ['styles', 'fonts', 'scripts'], function() {
+	return gulp.watch(['sass/**/*.scss', 'scripts/main.js'], ['styles', 'fonts', 'scripts']);
+});
---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+var Clipboard = require('clipboard');
+
+var outputTextBox = jQuery('#password-output');
+var generateButton = jQuery('#generate-btn');
+var toolTipContainer = outputTextBox.parent();
+
+toolTipContainer.tooltip({
+	title: 'Copied!',
+	trigger: 'manual',
+	placement: 'right',
+});
+
+function generate(length)
+{
+	var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	var n = charset.length;
+	var result = '';
+
+	for (var i = 0; i < length; i++)
+		result += charset.charAt(Math.floor(Math.random() * n));
+
+	return result;
+}
+
+function newPassword()
+{
+	var password = generate(16);
+	outputTextBox.val(password);
+}
+
+generateButton.click(function() {
+	newPassword();
+});
+
+outputTextBox.on('blur', function() {
+	toolTipContainer.tooltip('hide');
+});
+
+var clipboard = new Clipboard('#password-output');
+clipboard.on('success', function(e) {
+	toolTipContainer.tooltip('show');
+});
+
+newPassword();
